a2e5b7f5f10536c53ab5b2d9b3e211b2c35b0d23
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2022-12-22 17:02:45 +0500

    Initial commit

---
+++ src/main/java/com/example/backendspring/BackEndSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.backendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndSpringApplication.class, args);
+    }
+}
