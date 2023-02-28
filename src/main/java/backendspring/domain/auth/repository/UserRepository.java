0e05ca3e8f59bb5cd2381bac984b1cc961b63002
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2023-02-28 20:59:01 +0500

    feat: авторизация 4

---
+++ src/main/java/backendspring/domain/auth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package backendspring.domain.auth.repository;
+
+import backendspring.domain.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByUserName(String userName);
+
+	User findByEmail(String email);
+}
---
+++ src/main/java/backendspring/domain/auth/service/UserService.java
@@ -0,0 +1,43 @@
+package backendspring.domain.auth.service;
+
+import backendspring.domain.auth.model.Role;
+import backendspring.domain.auth.model.User;
+import backendspring.domain.auth.repository.RoleRepository;
+import backendspring.domain.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public User findUserByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	public User findUserByUserName(String userName) {
+		return userRepository.findByUserName(userName);
+	}
+
+	public User saveUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setActive(true);
+		Role userRole = roleRepository.findByRole("ADMIN");
+		user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+		return userRepository.save(user);
+	}
+
+}
