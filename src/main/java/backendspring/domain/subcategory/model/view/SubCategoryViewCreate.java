a2a6d876aef9f1b94b780b5aede951209aaae798
Alexey <stud0000250583@study.utmn.ru>
2023-04-08 15:50:29 +0500

    fix: POST on Product now works

--- src/main/java/backendspring/domain/subcategory/model/view/SubCategoryViewCreate.java
+++ src/main/java/backendspring/domain/subcategory/model/view/SubCategoryViewCreate.java
@@ -1,5 +1,6 @@
 package backendspring.domain.subcategory.model.view;
 
+import backendspring.domain.category.model.view.CategoryViewRead;
 import backendspring.infrasructure.view.ReferenceView;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,6 @@ public class SubCategoryViewCreate {
 
     private String name;
 
-    private ReferenceView parentCategory;
+    private CategoryViewRead parentCategory;
 
 }
