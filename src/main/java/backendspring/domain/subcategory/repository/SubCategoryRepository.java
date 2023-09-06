7b4ac2a144d6c4872ea434bb2dbe313050061586
mcdodik20008 <100521935+mcdodik20008@users.noreply.github.com>
2023-09-06 15:32:40 +0500

    занимаюсь фигней - 1

--- src/main/java/backendspring/domain/subcategory/repository/SubCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package backendspring.domain.subcategory.repository;
-
-import backendspring.domain.subcategory.model.entity.SubCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface SubCategoryRepository extends JpaRepository<SubCategory, Long>, QuerydslPredicateExecutor<SubCategory> {
-
-}
--- src/main/java/backendspring/domain/userorder/repository/ProductOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package backendspring.domain.userorder.repository;
-
-import backendspring.domain.userorder.model.entity.ProductOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long>, QuerydslPredicateExecutor<ProductOrder> {
-
-}
